e5398508570be2ab336223d31d1a756b9d2db144
Tim Court <tctimmeh@gmail.com>
2015-02-15 07:03:01 -0700

    added buttons to accept/reject an invitation

--- whosgoingsite/whosgoing/tests/unit/views/test_invitation.py
+++ whosgoingsite/whosgoing/tests/unit/views/test_invitation.py
@@ -1,9 +1,11 @@
+from allauth.account.models import EmailAddress
 from django.core.urlresolvers import reverse
 from whosgoing.tests.unit.whosGoingUnitTestCase import WhosGoingUnitTestCase
 
 
 class TestInvitationView(WhosGoingUnitTestCase):
     def setUp(self):
+        super().setUp()
         self.invitation = self.create_invitation()
 
     def get_url(self):
@@ -16,3 +18,16 @@ class TestInvitationView(WhosGoingUnitTestCase):
     def test_pageRenderedWithInvitation(self):
         self.get()
         self.assertLastContextValueEqual('invitation', self.invitation)
+
+    def test_indicatesInvitationForLoggedInUser(self):
+        user = self.logInAs()
+        EmailAddress.objects.create(user=user, email=self.invitation.address)
+        self.get()
+        self.assertLastContextValueEqual('inviteForUser', True)
+
+    def test_indicatesInvitationNotForLoggedInUserWithWrongEmail(self):
+        user = self.logInAs()
+        EmailAddress.objects.create(user=user, email=self.randStr()+'@host.com')
+        self.get()
+        self.assertLastContextValueEqual('inviteForUser', False)
+
--- whosgoingsite/whosgoing/views/invitiation.py
+++ whosgoingsite/whosgoing/views/invitiation.py
@@ -1,3 +1,4 @@
+from allauth.account.models import EmailAddress
 from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 from whosgoing.models import Invitation
@@ -7,7 +8,15 @@ class InvitationView(TemplateView):
     template_name = 'whosgoing/invitation.html'
 
     def get_context_data(self, inviteId, **kwargs):
+        data = {}
+
         invitation = get_object_or_404(Invitation, inviteId=inviteId)
-        return {'invitation': invitation}
+        data['invitation'] = invitation
+
+        if self.request.user is not None:
+            userEmails = EmailAddress.objects.filter(user__id=self.request.user.id, email=invitation.address)
+            data['inviteForUser'] = len(userEmails) > 0
+
+        return data
 
 invitationView = InvitationView.as_view()
