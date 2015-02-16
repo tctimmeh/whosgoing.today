a53f52be9fe850ef54267a8f4c30f292a022fe1b
Tim Court <tctimmeh@gmail.com>
2015-02-16 09:49:33 -0700

    home page lists user's events

--- whosgoingsite/whosgoing/views/home.py
+++ whosgoingsite/whosgoing/views/home.py
@@ -4,4 +4,9 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
     template_name = 'whosgoing/home.html'
 
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['events'] = self.request.user.events.order_by('name')
+        return data
+
 homeView = HomeView.as_view()
