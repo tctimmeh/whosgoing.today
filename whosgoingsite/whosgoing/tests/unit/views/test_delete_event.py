4b89d799728acb1ecf2ad1da1d229ce6bef3b9c9
Tim Court <tctimmeh@gmail.com>
2015-02-21 08:09:28 -0700

    unit tests for event edit/delete permissions

---
+++ whosgoingsite/whosgoing/tests/unit/views/test_delete_event.py
@@ -0,0 +1,19 @@
+from django.core.urlresolvers import reverse
+from whosgoing.tests.unit.requiresPermission import RequiresPermission
+from whosgoing.tests.unit.whosGoingUnitTestCase import WhosGoingUnitTestCase
+
+
+class TestDeleteEventView(WhosGoingUnitTestCase, RequiresPermission):
+    def setUp(self):
+        super().setUp()
+        self.event = self.create_event()
+
+    def get_url(self):
+        return reverse('eventDelete', kwargs={'id': self.event.id})
+
+    def add_permission(self):
+        self.event.add_member(self.loggedInUser)
+
+    def remove_permission(self):
+        self.event.remove_member(self.loggedInUser)
+
