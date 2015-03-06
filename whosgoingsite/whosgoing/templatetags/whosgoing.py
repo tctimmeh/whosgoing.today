5fc1885bd17bfb60f779e9e3af50a360f1e1febb
Tim Court <tctimmeh@gmail.com>
2015-03-06 16:28:04 -0700

    moved event page styles to own stylesheets; prod site expects combined, minified, global stylesheet

--- whosgoingsite/whosgoing/templatetags/whosgoing.py
+++ whosgoingsite/whosgoing/templatetags/whosgoing.py
@@ -10,3 +10,9 @@ def min_script(script_name):
         return '<script src="/static/js/{}"></script>'.format(script_name)
     return ''
 
+@register.simple_tag
+def min_style(style_name):
+    if getattr(settings, 'DEBUG', False):
+        return '<link rel="stylesheet" href="/static/css/{}" />'.format(style_name)
+    return ''
+
