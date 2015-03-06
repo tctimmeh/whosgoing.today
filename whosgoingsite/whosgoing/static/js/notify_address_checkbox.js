a91524fc65673c8b9a71137a3f4c122084a18721
Tim Court <tctimmeh@gmail.com>
2015-03-06 15:15:30 -0700

    js address notify checkbox class is proper jquery plugin

--- whosgoingsite/whosgoing/static/js/notify_address_checkbox.js
+++ whosgoingsite/whosgoing/static/js/notify_address_checkbox.js
@@ -1,37 +1,43 @@
-var whosgoing = whosgoing || {};
+(function($) {
+    NotifyAddressCheckbox = function(element) {
+        this.element = element;
+        this.address = this.element.data('address');
 
-whosgoing.NotifyAddressCheckbox = function(element) {
-    this.element = element;
-    this.address = this.element.data('address');
+        var self = this;
+        this.element.click(function(e){ self.clicked(e); });
+    };
 
-    var self = this;
-    this.element.click(function(e){ self.clicked(e); });
-};
+    NotifyAddressCheckbox.prototype.clicked = function(event) {
+        event.stopPropagation();
+        this.showWaitIndicator();
+        this.sendToggleRequest();
+    };
 
-whosgoing.NotifyAddressCheckbox.prototype.clicked = function(event) {
-    event.stopPropagation();
-    this.showWaitIndicator();
-    this.sendToggleRequest();
-};
+    NotifyAddressCheckbox.prototype.showWaitIndicator = function() {
+        var indicator = this.element.find('.indicator');
+        indicator.removeClass('glyphicon-ok glyphicon-remove text-success text-danger');
+        indicator.addClass('glyphicon-time');
+    };
 
-whosgoing.NotifyAddressCheckbox.prototype.showWaitIndicator = function() {
-    var indicator = this.element.find('.indicator');
-    indicator.removeClass('glyphicon-ok glyphicon-remove text-success text-danger');
-    indicator.addClass('glyphicon-time');
-};
+    NotifyAddressCheckbox.prototype.sendToggleRequest = function() {
+        $('<form />').ajaxSubmit({
+            type: 'POST',
+            url: toggleNotifyAddressUrl,
+            data: {
+                csrfmiddlewaretoken: $.cookie('csrftoken'),
+                address: this.address
+            },
+            target: this.element,
+            error: function (xhr, status, response, form) {
+                console.log('error', xhr, status, response, form);
+            }
+        });
+    };
 
-whosgoing.NotifyAddressCheckbox.prototype.sendToggleRequest = function() {
-    $('<form />').ajaxSubmit({
-        type: 'POST',
-        url: toggleNotifyAddressUrl,
-        data: {
-            csrfmiddlewaretoken: $.cookie('csrftoken'),
-            address: this.address
-        },
-        target: this.element,
-        error: function (xhr, status, response, form) {
-            console.log('error', xhr, status, response, form);
-        }
-    });
-};
+    $.fn.notifyAddressCheckbox = function() {
+        return this.each(function(index, obj) {
+            new NotifyAddressCheckbox($(obj));
+        });
+    };
+}(jQuery));
 
