9d080fca17c594cc7066d409a25378fa17e5cc00
Tim Court <tctimmeh@gmail.com>
2015-03-06 22:48:36 -0700

    edit event in ajax form

--- whosgoingsite/whosgoing/views/event/eventUpdate.py
+++ whosgoingsite/whosgoing/views/event/eventUpdate.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
+from django.core.urlresolvers import reverse
 from django.forms.utils import to_current_timezone
+from django.http import JsonResponse
 from django.utils.translation import ugettext as _
 from django.views.generic import UpdateView
 from whosgoing.forms.event import EventForm
@@ -10,7 +12,7 @@ from whosgoing.models import Event
 class EventUpdateView(UpdateView):
     model = Event
     pk_url_kwarg = 'eventId'
-    template_name = 'whosgoing/pages/event_update.html'
+    template_name = 'dcbase/form/popup-form.html'
     form_class = EventForm
 
     def get_initial(self):
@@ -18,8 +20,9 @@ class EventUpdateView(UpdateView):
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
-        data['title'] = _('%(eventName)s - Edit') % {'eventName': self.object.name}
-        data['submitText'] = _('Update')
+        data['form_url'] = reverse('whosgoing:event:update', kwargs={'eventId': self.object.id})
+        data['dialog_title'] = _('%(eventName)s - Edit') % {'eventName': self.object.name}
+        data['submit_text'] = _('Update')
         return data
 
     def get_object(self, queryset=None):
@@ -27,6 +30,10 @@ class EventUpdateView(UpdateView):
         if not event.is_member(self.request.user):
             raise PermissionDenied()
         return event
+
+    def form_valid(self, form):
+        super().form_valid(form)
+        return JsonResponse({'action': 'reload'})
 
 
 eventUpdateView = login_required(EventUpdateView.as_view())
