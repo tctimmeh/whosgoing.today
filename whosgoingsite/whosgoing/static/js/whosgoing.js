dc94a2e4588b8866c56e51fc313371e0bd8aa1ab
Tim Court <tctimmeh@gmail.com>
2015-03-13 13:12:25 -0600

    invite list shows who sent invite in tooltip

--- whosgoingsite/whosgoing/static/js/whosgoing.js
+++ whosgoingsite/whosgoing/static/js/whosgoing.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-   $('.time-zone-hint').popover();
+    $('.time-zone-hint').popover();
+    $('[data-toggle="tooltip"]').tooltip({
+        delay: 350,
+        placement: 'auto'
+    });
 });
