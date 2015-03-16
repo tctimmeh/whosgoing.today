8d3a9f2744917e873fc3d4ffaffcfc972de38669
Tim Court <tctimmeh@gmail.com>
2015-03-15 21:37:59 -0600

    restyled a bunch of zinnia pages

--- whosgoingsite/whosgoingsite/settings_dev.py
+++ whosgoingsite/whosgoingsite/settings_dev.py
@@ -6,6 +6,10 @@ SECRET_KEY = '0k&(29rhh!ler7e9wg0c!0_@=25br*+)j2lp15j3n2o_69rduw'
 DEBUG = True
 TEMPLATE_DEBUG = True
 
+INSTALLED_APPS = INSTALLED_APPS + (
+    'debug_toolbar',
+)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -14,3 +18,5 @@ DATABASES = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ZINNIA_PAGINATION = 5
