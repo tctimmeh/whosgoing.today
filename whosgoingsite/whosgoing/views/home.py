2e047c078d19b37b7deda036808ec7b51c2b37eb
Tim Court <tctimmeh@gmail.com>
2015-03-16 18:58:35 -0600

    home page displays latest "news" blog post

--- whosgoingsite/whosgoing/views/home.py
+++ whosgoingsite/whosgoing/views/home.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
 from whosgoing.models import Invitation
+from zinnia.models import Entry, Category
 
 
 class HomeView(TemplateView):
@@ -8,7 +9,19 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
         if not self.request.user.is_anonymous():
+            data['news'] = self._get_news()
             data['events'] = self.request.user.events.order_by('name')
         return data
+
+    def _get_news(self):
+        try:
+            newsCategory = Category.objects.get(slug='news')
+        except Category.DoesNotExist:
+            return None
+
+        news = newsCategory.entries_published().order_by('-creation_date')[:1]
+        if news:
+            return news[0]
+
 
 homeView = HomeView.as_view()
