991d0e347c79ec5e7c5f8c167891d9c63e51d057
Tim Court <tctimmeh@gmail.com>
2015-04-01 21:11:46 -0600

    restyled datetime widget

--- whosgoingsite/whosgoing/forms/occurrence.py
+++ whosgoingsite/whosgoing/forms/occurrence.py
@@ -4,12 +4,10 @@ from whosgoing.models import EventOccurrence
 
 class OccurrenceForm(ModelForm):
     time = SplitDateTimeField(input_time_formats=['%I:%M %p'],
-                              widget=SplitDateTimeWidget(attrs={'class': 'split-date-time'})
+                              widget=SplitDateTimeWidget(attrs={'class': 'split-date-time'}),
+                              label='Occurrence Date and Time'
     )
 
     class Meta:
         model = EventOccurrence
         fields = ['time']
-        labels = {
-            'time': 'Occurrence Time',
-        }
